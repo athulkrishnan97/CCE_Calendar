33f62f807485bf88e475147193911fd5ef751f22
Athul Krishnan <athulkrishnan97@gmail.com>
2019-08-03 00:30:22 +0530

    Initial commit

---
+++ sample/src/main/java/sema4/com/CCE_HOLISTIC_CALENDAR/CompactCalendarTab.java
@@ -0,0 +1,310 @@
+package sema4.com.CCE_HOLISTIC_CALENDAR;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class CompactCalendarTab extends Fragment {
+
+    private static final String TAG = "MainActivity";
+    private Calendar currentCalender = Calendar.getInstance(Locale.getDefault());
+    private SimpleDateFormat dateFormatForDisplaying = new SimpleDateFormat("dd-M-yyyy hh:mm:ss a", Locale.getDefault());
+    private SimpleDateFormat dateFormatForMonth = new SimpleDateFormat("MMM - yyyy", Locale.getDefault());
+    private boolean shouldShow = false;
+    private CompactCalendarView compactCalendarView;
+    private ActionBar toolbar;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View mainTabView = inflater.inflate(R.layout.main_tab,container,false);
+
+        final List<String> mutableBookings = new ArrayList<>();
+//        currentCalender.set(2019,Calendar.JULY,1);
+        final ListView bookingsListView = mainTabView.findViewById(R.id.bookings_listview);
+        final Button showPreviousMonthBut = mainTabView.findViewById(R.id.prev_button);
+        final Button showNextMonthBut = mainTabView.findViewById(R.id.next_button);
+        final Button slideCalendarBut = mainTabView.findViewById(R.id.slide_calendar);
+        final Button showCalendarWithAnimationBut = mainTabView.findViewById(R.id.show_with_animation_calendar);
+        final Button setLocaleBut = mainTabView.findViewById(R.id.set_locale);
+        final Button removeAllEventsBut = mainTabView.findViewById(R.id.remove_all_events);
+
+        final ArrayAdapter adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, mutableBookings);
+
+        bookingsListView.setAdapter(adapter);
+        compactCalendarView = mainTabView.findViewById(R.id.compactcalendar_view);
+
+//        compactCalendarView.set;
+
+        // below allows you to configure color for the current day in the month
+        // compactCalendarView.setCurrentDayBackgroundColor(getResources().getColor(R.color.black));
+        // below allows you to configure colors for the current day the user has selected
+         compactCalendarView.setCurrentSelectedDayBackgroundColor(getResources().getColor(R.color.light_blue));
+        compactCalendarView.setUseThreeLetterAbbreviation(false);
+        compactCalendarView.setFirstDayOfWeek(Calendar.MONDAY);
+        compactCalendarView.setIsRtl(false);
+        compactCalendarView.displayOtherMonthDays(false);
+        //compactCalendarView.setIsRtl(true);
+//        loadEvents();
+        loadEventsForYear(2019);
+        compactCalendarView.invalidate();
+
+        logEventsByMonth(compactCalendarView);
+
+        // below line will display Sunday as the first day of the week
+//         compactCalendarView.setShouldShowMondayAsFirstDay(false);
+
+        // disable scrolling calendar
+        // compactCalendarView.shouldScrollMonth(false);
+
+        // show days from other months as greyed out days
+        // compactCalendarView.displayOtherMonthDays(true);
+
+        // show Sunday as first day of month
+//         compactCalendarView.setShouldShowMondayAsFirstDay(false);
+//set initial title
+        toolbar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        toolbar.setTitle(dateFormatForMonth.format(compactCalendarView.getFirstDayOfCurrentMonth()));
+
+        //set title on calendar scroll
+        compactCalendarView.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+            @Override
+            public void onDayClick(Date dateClicked) {
+                toolbar.setTitle(dateFormatForMonth.format(dateClicked));
+                List<Event> bookingsFromMap = compactCalendarView.getEvents(dateClicked);
+                Log.d(TAG, "inside onclick " + dateFormatForDisplaying.format(dateClicked));
+                if (bookingsFromMap != null) {
+                    Log.d(TAG, bookingsFromMap.toString());
+                    mutableBookings.clear();
+                    for (Event booking : bookingsFromMap) {
+                        mutableBookings.add((String) booking.getData());
+                    }
+                    adapter.notifyDataSetChanged();
+                }
+
+            }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+                toolbar.setTitle(dateFormatForMonth.format(firstDayOfNewMonth));
+                onDayClick(firstDayOfNewMonth);
+            }
+        });
+
+        showPreviousMonthBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                compactCalendarView.scrollLeft();
+            }
+        });
+
+        showNextMonthBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                compactCalendarView.scrollRight();
+            }
+        });
+
+        final View.OnClickListener showCalendarOnClickLis = getCalendarShowLis();
+        slideCalendarBut.setOnClickListener(showCalendarOnClickLis);
+
+        final View.OnClickListener exposeCalendarListener = getCalendarExposeLis();
+        showCalendarWithAnimationBut.setOnClickListener(exposeCalendarListener);
+
+        compactCalendarView.setAnimationListener(new CompactCalendarView.CompactCalendarAnimationListener() {
+            @Override
+            public void onOpened() {
+            }
+
+            @Override
+            public void onClosed() {
+            }
+        });
+
+        setLocaleBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Locale locale = Locale.FRANCE;
+                dateFormatForDisplaying = new SimpleDateFormat("dd-M-yyyy hh:mm:ss a", locale);
+                TimeZone timeZone = TimeZone.getTimeZone("Europe/Paris");
+                dateFormatForDisplaying.setTimeZone(timeZone);
+                dateFormatForMonth.setTimeZone(timeZone);
+                compactCalendarView.setLocale(timeZone, locale);
+                compactCalendarView.setUseThreeLetterAbbreviation(false);
+//                loadEvents();
+                loadEventsForYear(2019);
+                logEventsByMonth(compactCalendarView);
+
+            }
+        });
+
+        removeAllEventsBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                compactCalendarView.removeAllEvents();
+            }
+        });
+
+
+        // uncomment below to show indicators above small indicator events
+        // compactCalendarView.shouldDrawIndicatorsBelowSelectedDays(true);
+
+        // uncomment below to open onCreate
+//        openCalendarOnCreate(v);
+
+        return mainTabView;
+    }
+
+    @NonNull
+    private View.OnClickListener getCalendarShowLis() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!compactCalendarView.isAnimating()) {
+                    if (shouldShow) {
+                        compactCalendarView.showCalendar();
+                    } else {
+                        compactCalendarView.hideCalendar();
+                    }
+                    shouldShow = !shouldShow;
+                }
+            }
+        };
+    }
+
+    @NonNull
+    private View.OnClickListener getCalendarExposeLis() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!compactCalendarView.isAnimating()) {
+                    if (shouldShow) {
+                        compactCalendarView.showCalendarWithAnimation();
+                    } else {
+                        compactCalendarView.hideCalendarWithAnimation();
+                    }
+                    shouldShow = !shouldShow;
+                }
+            }
+        };
+    }
+
+    private void openCalendarOnCreate(View v) {
+        final RelativeLayout layout = v.findViewById(R.id.main_content);
+        ViewTreeObserver vto = layout.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                if (Build.VERSION.SDK_INT < 16) {
+                    layout.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                } else {
+                    layout.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                }
+                compactCalendarView.showCalendarWithAnimation();
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        toolbar.setTitle(dateFormatForMonth.format(compactCalendarView.getFirstDayOfCurrentMonth()));
+        // Set to current day on resume to set calendar to latest day
+        // toolbar.setTitle(dateFormatForMonth.format(new Date()));
+    }
+
+    private void loadEvents() {
+        addEvents(-1, -1);
+        addEvents(Calendar.DECEMBER, -1);
+        addEvents(Calendar.AUGUST, -1);
+    }
+
+    private void loadEventsForYear(int year) {
+        for(int i=7;i<12;i++)
+        addEvents(i, 2019);
+    }
+
+    private void logEventsByMonth(CompactCalendarView compactCalendarView) {
+        currentCalender.setTime(new Date());
+        currentCalender.set(Calendar.DAY_OF_MONTH, 1);
+        currentCalender.set(Calendar.MONTH, Calendar.AUGUST);
+        List<String> dates = new ArrayList<>();
+        for (Event e : compactCalendarView.getEventsForMonth(new Date())) {
+            dates.add(dateFormatForDisplaying.format(e.getTimeInMillis()));
+        }
+        Log.d(TAG, "Events for Aug with simple date formatter: " + dates);
+        Log.d(TAG, "Events for Aug month using default local and timezone: " + compactCalendarView.getEventsForMonth(currentCalender.getTime()));
+    }
+
+    private void addEvents(int month, int year) {
+        currentCalender.setTime(new Date());
+        currentCalender.set(Calendar.DAY_OF_MONTH, 1);
+        int last_day= currentCalender.getActualMaximum(Calendar.DAY_OF_MONTH);
+        Date firstDayOfMonth = currentCalender.getTime();
+        for (int i = 0; i <last_day ; i++) {
+            currentCalender.setTime(firstDayOfMonth);
+            if (month > -1) {
+                currentCalender.set(Calendar.MONTH, month);
+            }
+            if (year > -1) {
+                currentCalender.set(Calendar.ERA, GregorianCalendar.AD);
+                currentCalender.set(Calendar.YEAR, year);
+            }
+            currentCalender.add(Calendar.DATE, i);
+            setToMidnight(currentCalender);
+            long timeInMillis = currentCalender.getTimeInMillis();
+
+//            List<Event> events = getEvents(timeInMillis, i);
+            String[][] data = new String[][] {{"COMMENCEMENT OF CLASSES","","VALUE ADDED PROGRAM","","","","","","FRESHERS DAY","","Holiday - BAKRID","","","","Holiday - INDEPENDENCE DAY","ADDON","ADDON","","MODULE TEST 1","","","","Holiday - SREEKRISHNA JAYANTHI","","","REMEDIAL CLASS","REMEDIAL CLASS","Holiday - AYANKALI JAYANTHI","","","ADDRESS BY PRINCIPAL/HOD"},
+                                            {"","SERIES 1","","","","ONAM CELEBRATION","Holiday - ONAM VACATION STARTS","","Holiday - MUHRAM","Holiday","Holiday","Holiday","Holiday","","","REOPENING, EXTRA ADDON","","","","PTA MEETING","Holiday - SREENARAYANA GURU SAMADHI","","REMEDIAL BASED ON SERIES 1","","","","FILM CLUB / LITERARY CLUB ACTIVITIES","","","",""},
+                                             {"","Holiday - GANDHI JAYANTHI","","VALUE ADDED PROGRAM","SPORTS DAY","","Holiday - MAHANAVAMI","","","","","","","SERIES II","","","","","SOCIALLY COMMITTED ACTIVITY","","REMEDIAL BASED ON SERIES II","","","","","","Holiday - DEEPAVALI","","","","", },
+                                             { "VALUE ADDED PROGRAM","","","","","SERIES III","","","","","","","","","CLASSES END","","","REMEDIAL CLASS STARTS","","","","","","","","","","","","","","","","","" },
+                                            {"","","","","","","","","","","","","","","","","","","","","","", "Holiday - CHRISTMAS VACATION STARTS","Holiday","Holiday - CHRISTMAS","Holiday","Holiday","","","","",}
+                                                    };
+            if(data[month-7][i]!="") {
+                int r=0,g=128,b=255;
+                if(data[month-7][i].contains("Holiday")){
+                    r=255;
+                    g=0;
+                    b=0;
+                }
+                List<Event> events = Arrays.asList(new Event(Color.argb(255, r, g, b), timeInMillis, data[month-7][i]));
+                compactCalendarView.addEvents(events);
+            }
+        }
+    }
+
+
+    private void setToMidnight(Calendar calendar) {
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+    }
+}
