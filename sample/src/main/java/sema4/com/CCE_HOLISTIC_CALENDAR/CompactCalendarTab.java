7a2b72fc179dd25112ff6e86cb0284aaccdd1bc0
Athul Krishnan <athulkrishnan97@gmail.com>
2019-08-05 02:05:14 +0530

    Firebase Added and checked

--- sample/src/main/java/sema4/com/CCE_HOLISTIC_CALENDAR/CompactCalendarTab.java
+++ sample/src/main/java/sema4/com/CCE_HOLISTIC_CALENDAR/CompactCalendarTab.java
@@ -17,6 +17,8 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.sundeepk.compactcalendarview.CompactCalendarView;
 import com.github.sundeepk.compactcalendarview.domain.Event;
@@ -31,6 +33,14 @@ import java.util.List;
 import java.util.Locale;
 import java.util.TimeZone;
 
+
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class CompactCalendarTab extends Fragment {
 
     private static final String TAG = "MainActivity";
@@ -40,10 +50,45 @@ public class CompactCalendarTab extends Fragment {
     private boolean shouldShow = false;
     private CompactCalendarView compactCalendarView;
     private ActionBar toolbar;
+    TextView text;
+    FirebaseDatabase database;
+    DatabaseReference myRef1;
+    DatabaseReference myRef2;
+    DatabaseReference myRef3;
+    DatabaseReference myRef4;
+    DatabaseReference myRef5;
+    ArrayList<String> month1=new ArrayList<>();
+    ArrayList<String> month2=new ArrayList<>();
+    ArrayList<String> month3=new ArrayList<>();
+    ArrayList<String> month4=new ArrayList<>();
+    ArrayList<String> month5=new ArrayList<>();
+
+
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View mainTabView = inflater.inflate(R.layout.main_tab,container,false);
+        text=mainTabView.findViewById(R.id.textView2);
+
+        database = FirebaseDatabase.getInstance();
+        myRef1 = database.getReference().child("august");
+        myRef2 = database.getReference().child("september");
+        myRef3 = database.getReference().child("october");
+        myRef4 = database.getReference().child("november");
+        myRef5 = database.getReference().child("december");
+
+
+       // myRef1.setValue("Hello, World!");
+
+
+        // [END write_message]
+
+        // [START read_message]
+        // Read from the da
+
 
         final List<String> mutableBookings = new ArrayList<>();
 //        currentCalender.set(2019,Calendar.JULY,1);
@@ -72,7 +117,287 @@ public class CompactCalendarTab extends Fragment {
         compactCalendarView.displayOtherMonthDays(false);
         //compactCalendarView.setIsRtl(true);
 //        loadEvents();
-        loadEventsForYear(2019);
+
+        myRef1.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                //String value = dataSnapshot.getValue(String.class);
+                // Log.d(TAG, "Value is: " + value);
+                //Toast.makeText(getContext(),value,Toast.LENGTH_LONG).show();
+                Post post1 = dataSnapshot.getValue(Post.class);
+
+
+
+
+                month1.add(post1.day1);
+                month1.add(post1.day2);
+                month1.add(post1.day3);
+                month1.add(post1.day4);
+                month1.add(post1.day5);
+                month1.add(post1.day6);
+                month1.add(post1.day7);
+                month1.add(post1.day8);
+                month1.add(post1.day9);
+                month1.add(post1.day10);
+                month1.add(post1.day11);
+                month1.add(post1.day12);
+                month1.add(post1.day13);
+                month1.add(post1.day14);
+                month1.add(post1.day15);
+                month1.add(post1.day16);
+                month1.add(post1.day17);
+                month1.add(post1.day18);
+                month1.add(post1.day19);
+                month1.add(post1.day20);
+                month1.add(post1.day21);
+                month1.add(post1.day22);
+                month1.add(post1.day23);
+                month1.add(post1.day24);
+                month1.add(post1.day25);
+                month1.add(post1.day26);
+                month1.add(post1.day27);
+                month1.add(post1.day28);
+                month1.add(post1.day29);
+                month1.add(post1.day30);
+                month1.add(post1.day31);
+
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+        myRef2.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                //String value = dataSnapshot.getValue(String.class);
+                // Log.d(TAG, "Value is: " + value);
+                //Toast.makeText(getContext(),value,Toast.LENGTH_LONG).show();
+                Post post2 = dataSnapshot.getValue(Post.class);
+
+
+
+
+                month2.add(post2.day1);
+                month2.add(post2.day2);
+                month2.add(post2.day3);
+                month2.add(post2.day4);
+                month2.add(post2.day5);
+                month2.add(post2.day6);
+                month2.add(post2.day7);
+                month2.add(post2.day8);
+                month2.add(post2.day9);
+                month2.add(post2.day10);
+                month2.add(post2.day11);
+                month2.add(post2.day12);
+                month2.add(post2.day13);
+                month2.add(post2.day14);
+                month2.add(post2.day15);
+                month2.add(post2.day16);
+                month2.add(post2.day17);
+                month2.add(post2.day18);
+                month2.add(post2.day19);
+                month2.add(post2.day20);
+                month2.add(post2.day21);
+                month2.add(post2.day22);
+                month2.add(post2.day23);
+                month2.add(post2.day24);
+                month2.add(post2.day25);
+                month2.add(post2.day26);
+                month2.add(post2.day27);
+                month2.add(post2.day28);
+                month2.add(post2.day29);
+                month2.add(post2.day30);
+                month2.add(post2.day31);
+
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+
+        myRef3.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                //String value = dataSnapshot.getValue(String.class);
+                // Log.d(TAG, "Value is: " + value);
+                //Toast.makeText(getContext(),value,Toast.LENGTH_LONG).show();
+                Post post3 = dataSnapshot.getValue(Post.class);
+
+                month3. add(post3.day1);
+                month3.add(post3.day2);
+                month3.add(post3.day3);
+                month3.add(post3.day4);
+                month3.add(post3.day5);
+                month3.add(post3.day6);
+                month3.add(post3.day7);
+                month3.add(post3.day8);
+                month3.add(post3.day9);
+                month3.add(post3.day10);
+                month3.add(post3.day11);
+                month3.add(post3.day12);
+                month3.add(post3.day13);
+                month3.add(post3.day14);
+                month3.add(post3.day15);
+                month3.add(post3.day16);
+                month3.add(post3.day17);
+                month3.add(post3.day18);
+                month3.add(post3.day19);
+                month3.add(post3.day20);
+                month3.add(post3.day21);
+                month3.add(post3.day22);
+                month3.add(post3.day23);
+                month3.add(post3.day24);
+                month3.add(post3.day25);
+                month3.add(post3.day26);
+                month3.add(post3.day27);
+                month3.add(post3.day28);
+                month3.add(post3.day29);
+                month3.add(post3.day30);
+                month3.add(post3.day31);
+
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+
+        myRef4.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                //String value = dataSnapshot.getValue(String.class);
+                // Log.d(TAG, "Value is: " + value);
+                //Toast.makeText(getContext(),value,Toast.LENGTH_LONG).show();
+                Post post4 = dataSnapshot.getValue(Post.class);
+
+                month4. add(post4.day1);
+                month4.add(post4.day2);
+                month4.add(post4.day3);
+                month4.add(post4.day4);
+                month4.add(post4.day5);
+                month4.add(post4.day6);
+                month4.add(post4.day7);
+                month4.add(post4.day8);
+                month4.add(post4.day9);
+                month4.add(post4.day10);
+                month4.add(post4.day11);
+                month4.add(post4.day12);
+                month4.add(post4.day13);
+                month4.add(post4.day14);
+                month4.add(post4.day15);
+                month4.add(post4.day16);
+                month4.add(post4.day17);
+                month4.add(post4.day18);
+                month4.add(post4.day19);
+                month4.add(post4.day20);
+                month4.add(post4.day21);
+                month4.add(post4.day22);
+                month4.add(post4.day23);
+                month4.add(post4.day24);
+                month4.add(post4.day25);
+                month4.add(post4.day26);
+                month4.add(post4.day27);
+                month4.add(post4.day28);
+                month4.add(post4.day29);
+                month4.add(post4.day30);
+                month4.add(post4.day31);
+
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+        myRef5.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                //String value = dataSnapshot.getValue(String.class);
+                // Log.d(TAG, "Value is: " + value);
+                //Toast.makeText(getContext(),value,Toast.LENGTH_LONG).show();
+                Post post5 = dataSnapshot.getValue(Post.class);
+
+
+
+                month5. add(post5.day1);
+                month5.add(post5.day2);
+                month5.add(post5.day3);
+                month5.add(post5.day4);
+                month5.add(post5.day5);
+                month5.add(post5.day6);
+                month5.add(post5.day7);
+                month5.add(post5.day8);
+                month5.add(post5.day9);
+                month5.add(post5.day10);
+                month5.add(post5.day11);
+                month5.add(post5.day12);
+                month5.add(post5.day13);
+                month5.add(post5.day14);
+                month5.add(post5.day15);
+                month5.add(post5.day16);
+                month5.add(post5.day17);
+                month5.add(post5.day18);
+                month5.add(post5.day19);
+                month5.add(post5.day20);
+                month5.add(post5.day21);
+                month5.add(post5.day22);
+                month5.add(post5.day23);
+                month5.add(post5.day24);
+                month5.add(post5.day25);
+                month5.add(post5.day26);
+                month5.add(post5.day27);
+                month5.add(post5.day28);
+                month5.add(post5.day29);
+                month5.add(post5.day30);
+                month5.add(post5.day31);
+
+                loadEventsForYear(2019);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+
+
+
+
+
         compactCalendarView.invalidate();
 
         logEventsByMonth(compactCalendarView);
@@ -280,14 +605,77 @@ public class CompactCalendarTab extends Fragment {
             setToMidnight(currentCalender);
             long timeInMillis = currentCalender.getTimeInMillis();
 
-//            List<Event> events = getEvents(timeInMillis, i);
-            String[][] data = new String[][] {{"COMMENCEMENT OF CLASSES","","VALUE ADDED PROGRAM","","","","","","FRESHERS DAY","","Holiday - BAKRID","","","","Holiday - INDEPENDENCE DAY","ADDON","ADDON","","MODULE TEST 1","","","","Holiday - SREEKRISHNA JAYANTHI","","","REMEDIAL CLASS","REMEDIAL CLASS","Holiday - AYANKALI JAYANTHI","","","ADDRESS BY PRINCIPAL/HOD"},
-                                            {"","SERIES 1","","","","ONAM CELEBRATION","Holiday - ONAM VACATION STARTS","","Holiday - MUHRAM","Holiday","Holiday","Holiday","Holiday","","","REOPENING, EXTRA ADDON","","","","PTA MEETING","Holiday - SREENARAYANA GURU SAMADHI","","REMEDIAL BASED ON SERIES 1","","","","FILM CLUB / LITERARY CLUB ACTIVITIES","","","",""},
-                                             {"","Holiday - GANDHI JAYANTHI","","VALUE ADDED PROGRAM","SPORTS DAY","","Holiday - MAHANAVAMI","","","","","","","SERIES II","","","","","SOCIALLY COMMITTED ACTIVITY","","REMEDIAL BASED ON SERIES II","","","","","","Holiday - DEEPAVALI","","","","", },
-                                             { "VALUE ADDED PROGRAM","","","","","SERIES III","","","","","","","","","CLASSES END","","","REMEDIAL CLASS STARTS","","","","","","","","","","","","","","","","","" },
-                                            {"","","","","","","","","","","","","","","","","","","","","","", "Holiday - CHRISTMAS VACATION STARTS","Holiday","Holiday - CHRISTMAS","Holiday","Holiday","","","","",}
-                                                    };
-            if(data[month-7][i]!="") {
+
+
+
+
+            //List<Event> events = getEvents(timeInMillis, i);
+//            String[][] data = new String[][] {{"COMMENCEMENT OF CLASSES","","VALUE ADDED PROGRAM","","","","","","FRESHERS DAY","","Holiday - BAKRID","","","","Holiday - INDEPENDENCE DAY","ADDON","ADDON","","MODULE TEST 1","","","","Holiday - SREEKRISHNA JAYANTHI","","","REMEDIAL CLASS","REMEDIAL CLASS","Holiday - AYANKALI JAYANTHI","","","ADDRESS BY PRINCIPAL/HOD"},
+//                                            {"","SERIES 1","","","","ONAM CELEBRATION","Holiday - ONAM VACATION STARTS","","Holiday - MUHRAM","Holiday","Holiday","Holiday","Holiday","","","REOPENING, EXTRA ADDON","","","","PTA MEETING","Holiday - SREENARAYANA GURU SAMADHI","","REMEDIAL BASED ON SERIES 1","","","","FILM CLUB / LITERARY CLUB ACTIVITIES","","","",""},
+//                                             {"","Holiday - GANDHI JAYANTHI","","VALUE ADDED PROGRAM","SPORTS DAY","","Holiday - MAHANAVAMI","","","","","","","SERIES II","","","","","SOCIALLY COMMITTED ACTIVITY","","REMEDIAL BASED ON SERIES II","","","","","","Holiday - DEEPAVALI","","","","", },
+//                                             { "VALUE ADDED PROGRAM","","","","","SERIES III","","","","","","","","","CLASSES END","","","REMEDIAL CLASS STARTS","","","","","","","","","","","","","","","","","" },
+//                                            {"","","","","","","","","","","","","","","","","","","","","","", "Holiday - CHRISTMAS VACATION STARTS","Holiday","Holiday - CHRISTMAS","Holiday","Holiday","","","","",}
+//                                                    };
+
+            String[][] data = new String[5][31];
+
+            int z=0;
+            for (String s:month1){
+
+                data[0][z]=s;
+
+
+
+                z++;
+
+
+            }
+            z=0;
+            for (String s:month2){
+
+                data[1][z]=s;
+
+                z++;
+            }
+            z=0;
+            for (String s:month3){
+
+                data[2][z]=s;
+                z++;
+            }
+            z=0;
+            for (String s:month4){
+
+                data[3][z]=s;
+                z++;
+            }
+            z=0;
+            for (String s:month5){
+
+                data[4][z]=s;
+                z++;
+
+            }
+
+
+            String yt;
+
+                for (int k = 0; k < 31; k++) {
+
+
+                   yt=data[2][k];
+                    Log.d(TAG, "////////////////////////////////////////////////////////////////////////////////////////////////");
+                    Log.d(TAG, ""+yt);
+
+                }
+
+
+
+
+
+
+
+            if(!data[month-7][i].equals("empty")) {
                 int r=0,g=128,b=255;
                 if(data[month-7][i].contains("Holiday")){
                     r=255;
