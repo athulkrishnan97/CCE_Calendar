f5869398fc2bea1cca98aae2e3af13d092c46a8b
Athul Krishnan <athulkrishnan97@gmail.com>
2019-08-10 21:24:02 +0530

    Splash screen added

---
+++ sample/src/main/java/sema4/com/CCE_HOLISTIC_CALENDAR/splash.java
@@ -0,0 +1,30 @@
+package sema4.com.CCE_HOLISTIC_CALENDAR;
+
+import android.os.Bundle;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+
+
+
+public class splash extends Activity {
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(splash.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+
+    }
+}
